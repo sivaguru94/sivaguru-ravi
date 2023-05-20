65fa14ad16502bc44b175a1999330ee51cde95bf
Sivaguru Ravi <sivaguru94@gmail.com>
2023-05-20 15:46:53 +0530

    Angular first commimt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-pofile';
+export class AppComponent implements OnInit {
+  config: any;
+  fullpage_api: any;
+
+  constructor() {
+
+    // this is just an example => for more details on config please visit fullPage.js docs
+    this.config = {
+      licenseKey: 'YOUR LICENSE KEY HERE',
+      sectionsColor: ['#7BAABE', 'whitesmoke', '#7BAABE', 'whitesmoke', '#7BAABE'],
+      anchors: ['p1', 'p2', 'p3', 'p4', 'p5'],
+      navigation: true,
+    };
+  }
+
+  ngOnInit() {
+  }
+
+  getRef(fullPageRef: any) {
+    this.fullpage_api = fullPageRef;
+  }
+
 }
